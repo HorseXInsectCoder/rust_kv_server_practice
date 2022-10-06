1bdb47ed14a66b5b8134f03426bc37e740f2a49b
voyager-1 <voyager-1@192.168.0.108>
2022-10-07 00:20:11 +0800

    fin

--- src/service/mod.rs
+++ src/service/mod.rs
@@ -1,8 +1,11 @@
 mod command_service;
 
+use std::sync::Arc;
+use tracing::debug;
 use crate::*;
 use crate::command_request::RequestData;
 use crate::errors::KvError;
+use crate::memory::MemTable;
 use crate::storage::Storage;
 
 // 未来我们支持新命令时，只需要做两件事：为命令实现 CommandService、在 dispatch 方法中添加新命令的支持
@@ -28,5 +31,88 @@ pub fn dispatch(cmd: CommandRequest, store: &impl Storage) -> CommandResponse {
         Some(RequestData::Hset(param)) => param.execute(store),
         None => KvError::InvalidCommand("Request has no data".into()).into(),
         _ => KvError::Internal("Not implemented".into()).into()
+    }
+}
+
+// Service 的声明和实现
+/// Service 数据结构
+pub struct Service<Store = MemTable> {
+    inner: Arc<ServiceInner<Store>>,
+}
+
+impl<Store> Clone for Service<Store> {
+    fn clone(&self) -> Self {
+        Self {
+            inner: Arc::clone(&self.inner),
+        }
+    }
+}
+
+/// Service 内部数据结构
+pub struct ServiceInner<Store> {
+    store: Store
+}
+
+impl<Store: Storage> Service<Store> {
+    pub fn new(store: Store) -> Self {
+        Self {
+            inner: Arc::new(ServiceInner { store })
+        }
+    }
+
+    pub fn execute(&self, cmd: CommandRequest) -> CommandResponse {
+        debug!("Got request: {:?}", cmd);
+        // TODO: 发送 on_received 事件
+        let res = dispatch(cmd, &self.inner.store);
+        debug!("Executed response: {:?}", res);
+
+        // TODO: 发送 on_executed 事件
+
+        res
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::thread;
+    use crate::{CommandRequest, CommandResponse, KvPair, Service, Value};
+    use crate::memory::MemTable;
+
+    #[test]
+    fn servicek_should_works() {
+        // 我们需要一个 service 结构至少包含 Storage
+        let service = Service::new(MemTable::default());
+
+        // service 可以运行在多线程环境下，它的 clone 应该是轻量级的
+        let cloned = service.clone();
+
+        // 创建一个线程，在 table t1 中写入 k1, v1
+        let handle = thread::spawn(move || {
+            let res = cloned.execute(CommandRequest::new_hset("t1", "k1", "v1".into()));
+            assert_res_ok(res, &["v1".into()], &[]);
+        });
+        handle.join().unwrap();
+
+        // 在当前线程下读取 table t1 的 k1，应该返回 v1
+        let res = service.execute(CommandRequest::new_hget("t1", "k1"));
+        assert_res_ok(res, &["v1".into()], &[]);
+    }
+
+
+    // 测试成功返回的结果
+    fn assert_res_ok(mut res: CommandResponse, values: &[Value], pairs: &[KvPair]) {
+        res.pairs.sort_by(|a, b| a.partial_cmp(b).unwrap());
+        assert_eq!(res.status, 200);
+        assert_eq!(res.message, "");
+        assert_eq!(res.values, values);    // res.values是Vec<abi::Value>，所以直接用切片更方便
+        assert_eq!(res.pairs, pairs);
+    }
+
+    // 测试失败返回的结果
+    fn assert_res_error(res: CommandResponse, code: u32, msg: &str) {
+        assert_eq!(res.status, code);
+        assert!(res.message.contains(msg));
+        assert_eq!(res.values, &[]);
+        assert_eq!(res.pairs, &[]);
     }
 }
